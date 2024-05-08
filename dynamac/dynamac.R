7aa1cbee461a3b38b369da20e4ac20be0f011f97
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-05-08 14:35:21 +0700

    Use dynamac (dynardl) With (In) R Software

--- dynamac/dynamac.R
+++ dynamac/dynamac.R
@@ -20,9 +20,6 @@ dynamac <- readxl::read_excel(temp_file)
 # Estimate Dynamic simulation and testing for single-equation ARDL models Use dynamac (dynardl) With (In) R Software
 dynamac<-dynardl(concern~incshare10+urate,data=dynamac,lags=list("concern"=1,"incshare10"=1),diffs=c("incshare10","urate"),ec=TRUE,simulate=FALSE)
 summary(dynamac)
-dynamac_2<-dynardl(concern~incshare10+ urate,data=dynamac,lags=list("concern"= 1,"incshare10"=1),diffs=c("incshare10","urate"),lagdiffs=list("concern"=1),
-ec=TRUE,simulate=FALSE)
-summary(dynamac_2)
 # Dynamic simulation and testing for single-equation ARDL models Use dynamac (dynardl) With (In) R Software
 # Olah Data Semarang
 # WhatsApp : +6285227746673
